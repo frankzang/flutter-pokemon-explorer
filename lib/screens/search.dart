1d7d1ea8734e1b72a1dfbfe272f5b76d81daf74b
Frank <fsantos.fs37@gmail.com>
2019-08-31 16:13:35 -0300

    adiciona indicador de pokemon não encontrado

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -15,6 +15,7 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   Pokemon _pokemon;
   bool _isSearching = false;
+  bool _notFound = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +27,17 @@ class _SearchPageState extends State<SearchPage> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 5, vertical: 40),
                 child: SearchInput(onFieldSubmitted: _searchPokemon)),
-            !_isSearching && _pokemon != null
+            !_isSearching
                 ? Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: _pokemon != null ? SearchResult(_pokemon) : null,
                   )
                 : Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: CircularProgressIndicator())
+                    child: CircularProgressIndicator()),
+            Center(
+              child: _notFound ? Text("Not found") : null,
+            )
           ],
         ),
       ),
@@ -45,6 +49,7 @@ class _SearchPageState extends State<SearchPage> {
 
     setState(() {
       _isSearching = true;
+      _notFound = false;
     });
 
     var path = "https://pokeapi.co/api/v2/pokemon/${text.toLowerCase()}";
@@ -54,6 +59,13 @@ class _SearchPageState extends State<SearchPage> {
       if (response.statusCode == 200 && mounted) {
         setState(() {
           _pokemon = Pokemon.fromJson(json.decode(response.body));
+          _isSearching = false;
+        });
+      }
+      if (response.statusCode == 404 && mounted) {
+        setState(() {
+          _pokemon = null;
+          _notFound = true;
           _isSearching = false;
         });
       }
