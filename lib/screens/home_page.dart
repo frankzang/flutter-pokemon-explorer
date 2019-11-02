8ca92ab2100a2b168f9c693f0d8c2fb640d706eb
Frank <fsantos.fs37@gmail.com>
2019-11-02 12:23:05 -0300

    remove algumas animações

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_explorer/models/pokemon_model.dart';
 import 'package:pokemon_explorer/widgets/pokemon_card.dart';
 import 'package:pokemon_explorer/widgets/pokemon_count.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:pokemon_explorer/config/api.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -12,7 +17,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _itemCount = 15;
+  final List<PokemonModel> pokemonList = [];
+
+  bool _isGettingPokemon = false;
+
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = new ScrollController();
+    getMorePokemon();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,22 +85,69 @@ class _HomePageState extends State<HomePage> {
                         crossAxisCount: 2),
                     delegate: SliverChildBuilderDelegate((context, index) {
                       return GridTile(
-                        child: PokemonCard(index + 1),
+                        child: PokemonCard(pokemonList[index]),
                       );
-                    }, childCount: _itemCount),
-                  )
+                    }, childCount: pokemonList.length),
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate([
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50, bottom: 100),
+                        child: Center(
+                          child: _isGettingPokemon
+                              ? CircularProgressIndicator()
+                              : null,
+                        ),
+                      )
+                    ]),
+                  ),
                 ],
               )),
         ));
   }
 
   bool _onScrollNotification(ScrollNotification scrollInfo) {
+    if (_isGettingPokemon) return null;
+
+    if (pokemonList.length < 1) return null;
+
     if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
-      setState(() {
-        _itemCount = _itemCount + 5;
-      });
-      return null;
+      getMorePokemon();
     }
     return null;
+  }
+
+  getMorePokemon() async {
+    setState(() {
+      _isGettingPokemon = true;
+    });
+
+    var length = pokemonList.length;
+
+    for (var i = length + 1; i < length + 10; i++) {
+      PokemonModel pokemon = await getPokemon(i);
+
+      if (pokemon != null) {
+        pokemonList.add(pokemon);
+      }
+    }
+
+    setState(() {
+      _isGettingPokemon = false;
+    });
+  }
+
+  Future<PokemonModel> getPokemon(int _pokemonNumber) async {
+    var path = "$apiUrl${_pokemonNumber.toString()}";
+
+    try {
+      var response = await http.get(path);
+      if (response.statusCode == 200 && mounted) {
+        return PokemonModel.fromJson(json.decode(response.body));
+      }
+      return null;
+    } catch (e) {
+      return null;
+    }
   }
 }
--- lib/widgets/pokemon_card.dart
+++ lib/widgets/pokemon_card.dart
@@ -1,15 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:pokemon_explorer/config/api.dart';
 import 'package:pokemon_explorer/models/pokemon_model.dart';
 import 'package:pokemon_explorer/screens/pokemon_details_page.dart';
+import 'package:pokemon_explorer/utils/capitalize.dart';
 
 class PokemonCard extends StatefulWidget {
-  final int _pokemonNumber;
+  final PokemonModel _pokemon;
 
-  PokemonCard(this._pokemonNumber);
+  const PokemonCard(this._pokemon);
 
   @override
   State<StatefulWidget> createState() {
@@ -19,114 +17,72 @@ class PokemonCard extends StatefulWidget {
 
 class _PokemonCardState extends State<PokemonCard>
     with TickerProviderStateMixin {
-  AnimationController _controller;
-  Animation _animation;
-  PokemonModel _pokemon;
-
-  @override
-  void initState() {
-    _controller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    getPokemon();
-
-    return FadeTransition(
-      opacity: _animation,
-      child: Card(
-          elevation: 0,
-          color: Colors.grey[200],
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          clipBehavior: Clip.hardEdge,
-          child: InkWell(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: _pokemon != null
-                  ? Container(
-                      child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Hero(
-                            tag: "pokemon_hero${widget._pokemonNumber}",
-                            child: SingleChildScrollView(
-                              physics: NeverScrollableScrollPhysics(),
-                              padding: EdgeInsets.all(8),
-                              child: SizedBox(
-                                height: 80,
-                                width: 80,
-                                child: CachedNetworkImage(
-                                  fit: BoxFit.cover,
-                                  height: 70,
-                                  imageUrl: _pokemon.sprites["front_default"],
-                                  placeholder: (context, url) =>
-                                      CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) =>
-                                      Icon(Icons.error),
-                                ),
+    return Card(
+        elevation: 0,
+        color: Colors.grey[200],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        clipBehavior: Clip.hardEdge,
+        child: InkWell(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: widget._pokemon != null
+                ? Container(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Hero(
+                          tag: "pokemon_hero${widget._pokemon.id}",
+                          child: SingleChildScrollView(
+                            physics: NeverScrollableScrollPhysics(),
+                            padding: EdgeInsets.all(8),
+                            child: SizedBox(
+                              height: 80,
+                              width: 80,
+                              child: CachedNetworkImage(
+                                fit: BoxFit.cover,
+                                height: 70,
+                                imageUrl:
+                                    widget._pokemon.sprites["front_default"],
+                                placeholder: (context, url) =>
+                                    CircularProgressIndicator(),
+                                errorWidget: (context, url, error) =>
+                                    Icon(Icons.error),
                               ),
-                            )),
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 5),
-                          child: Text(
-                            "N°${_pokemon.id.toString()}",
-                            style: TextStyle(
-                                color: Colors.black38,
-                                fontFamily: "Yanone",
-                                fontSize: 12,
-                                letterSpacing: .5,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Text(
-                          _pokemon.name.substring(0, 1).toUpperCase() +
-                              _pokemon.name.substring(1),
+                            ),
+                          )),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 5),
+                        child: Text(
+                          "N°${widget._pokemon.id.toString()}",
                           style: TextStyle(
-                              color: Colors.black54,
-                              letterSpacing: .8,
+                              color: Colors.black38,
+                              fontFamily: "Yanone",
+                              fontSize: 12,
+                              letterSpacing: .5,
                               fontWeight: FontWeight.bold),
                         ),
-                      ],
-                    ))
-                  : null,
-            ),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          PokemonDetails(_pokemon, widget._pokemonNumber)));
-            },
-          )),
-    );
-  }
-
-  getPokemon() async {
-    if (_pokemon != null) {
-      return;
-    }
-
-    var path = "$apiUrl${widget._pokemonNumber}";
-
-    try {
-      var response = await http.get(path);
-      if (response.statusCode == 200 && mounted) {
-        setState(() {
-          _pokemon = PokemonModel.fromJson(json.decode(response.body));
-        });
-        _controller.forward();
-      }
-    } catch (e) {}
+                      ),
+                      Text(
+                        capitalize(widget._pokemon.name),
+                        style: TextStyle(
+                            color: Colors.black54,
+                            letterSpacing: .8,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ))
+                : null,
+          ),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        PokemonDetails(widget._pokemon, widget._pokemon.id)));
+          },
+        ));
   }
 }
